d038bfce4812c1ab9f8ee38eaacc00912e910225
Karen <karenpoghosyan047@gmail.com>
2023-12-18 15:19:26 +0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+// import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle';
+var swiper = new Swiper(".mySwiper1", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+var swiper = new Swiper(".mySwiper2", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
+});
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  rewind: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+var swiper = new Swiper(".mySwiper3", {
+  rewind: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+
+
+  
---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+let mix = require('laravel-mix');
+
+mix.sass('src/scss/style.scss','dist/css');
+mix.sass('src/scss/lib.scss','dist/css');
+
+mix.js('src/js/script.js','dist/js');
